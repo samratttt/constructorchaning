a449aca405df7b6750a7e3ad40b286301b167168
samrat-mdit <karki.samrat.mdit@gmail.com>
2023-03-25 22:56:04 +0545

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+
+    // using this()
+    public Main() {
+
+        this("samrat");   // invokes another class of same constructor of same class
+        System.out.print(" karki");
+
+    }
+
+    public Main(String str) {
+        System.out.print(str);
+    }
+
+}
+
+class Child extends Main {
+    Child() {
+        super();  // invokes the super class constructor : can be default or parameterized
+        System.out.println(" is from nepal");
+    }
+
+    public static void main(String[] args) {
+        Child obj = new Child();
+    }
+}
